60d8169466c62ba8b827304c97025f9e0049e8a0
ismael-valdivia-D <isma49345@gmail.com>
2026-04-17 18:10:25 -0400

    save

---
+++ for.py
@@ -0,0 +1,28 @@
+# num = int(input("cual tabla de multiplicar quiere?"))
+
+# for i in range(1, 11): 
+#     print(i, " X ", num, " = ", (num * i))
+#     print(f"{num}x{i}={num*i}")
+# # # con manipulacion de limites de range 
+# # for i in range(1,11):
+# #     print(f"{num}x{i}={num*i}")
+
+# #preguntar cuantas son
+# #y sacar el promedio de ellas
+
+# cantidad = int(input("cuantas notas quieres ingresar?"))
+# suma = 0
+# for i in range(cantidad):
+#     nota = float(input("ingresa la nota: "))
+#     suma += nota
+
+# promedio = suma / cantidad
+# print(f"el promedio es: {promedio}")
+letras=0
+nombre = input("cual es tu nombre? ")
+for i in nombre:
+    print(i)
+    letras += 1
+    print(f"el total de letras es de {letras}")
+
+
--- if.py
+++ if.py
@@ -1,1 +1,13 @@
-print("Hola Pipe")
+titulo = "clima de hoy" #string
+diaDelMes = 17 #int
+Mes=4 #int
+temperatura = 20.5 #float
+llueve = False #boolean
+print(titulo, "temperatura: ", temperatura,diaDelMes,"/", Mes )
+
+temperatura>28 ---->False
+mes==4--->True
+if llueve == True:
+    print("lleva paraguas")
+else :
+    print("no hace falta paraguas")
